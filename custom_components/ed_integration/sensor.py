9d38c727dba9d68ce037baa32d43131bc6284da6
Noah Kottenhahn <noah.kottenhahn@stud.h-da.de>
2020-10-25 19:16:09 +0100

    remove entity class, use central sensor class instead

--- custom_components/ed_integration/sensor.py
+++ custom_components/ed_integration/sensor.py
@@ -1,23 +1,52 @@
 """Sensor platform for ed_integration."""
-from custom_components.ed_integration.const import DOMAIN, ICON_LOCATION
-from custom_components.ed_integration.entity import EDEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import KEY_CMDR_NAME, KEY_OUTPUT_LOCATION_STR
+from custom_components.ed_integration.const import DOMAIN, ICON_LOCATION
+
+from .const import KEY_CMDR_NAME, KEY_OUTPUT_LOCATION_STR, NAME, VERSION
 
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_devices([EDLocationSensor(coordinator, entry)])
+    cmdr_name = entry.data.get(KEY_CMDR_NAME)
+    async_add_devices([EDLocationSensor(coordinator, cmdr_name)])
 
 
-class EDLocationSensor(EDEntity):
+class EDLocationSensor(CoordinatorEntity):
     """ed_integration Sensor class."""
+
+    def __init__(self, coordinator, cmdr_name):
+        super().__init__(coordinator)
+        self._cmdr_name = cmdr_name
+
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this entity."""
+        cmdr_name_id = self._cmdr_name.replace(" ", "_")
+        return f"{cmdr_name_id}_location"
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f"CMDR {self.config_entry.data.get(KEY_CMDR_NAME)}"
+        return f"CMDR {self._cmdr_name}"
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "name": NAME,
+            "model": VERSION,
+            "manufacturer": NAME,
+        }
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return {
+            "static": self.coordinator.data.get("static"),
+            "time": self.coordinator.data.get("time")
+        }
 
     @property
     def state(self):
